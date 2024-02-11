b39764ddfd0388a23ad01f97a1faafb07f2fcf5d
susankoju <askusankoju@gmail.com>
2024-02-11 16:16:11 +0545

    Updates

---
+++ precache-manifest.c505b815de450b3237aee6e18f27a0d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e56609f6539a1ab0ccfb5b4425706a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8937c6401b08a66d34e",
+    "url": "/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "f6e82c9a12285a6b621c",
+    "url": "/static/css/main.a34fc806.chunk.css"
+  },
+  {
+    "revision": "c8937c6401b08a66d34e",
+    "url": "/static/js/2.32a1183b.chunk.js"
+  },
+  {
+    "revision": "7981a8fdc46b54813955",
+    "url": "/static/js/3.0d74b8aa.chunk.js"
+  },
+  {
+    "revision": "f6e82c9a12285a6b621c",
+    "url": "/static/js/main.dc7b83b2.chunk.js"
+  },
+  {
+    "revision": "c3e2eb17818b9ac245c5",
+    "url": "/static/js/runtime~main.8e5553dd.js"
+  }
+]);
