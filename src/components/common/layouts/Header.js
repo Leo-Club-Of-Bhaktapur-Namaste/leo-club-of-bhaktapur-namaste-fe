4c60982b24b756f3f9fa89a25f856104942231c6
susankoju <askusankoju@gmail.com>
2024-02-11 16:38:55 +0545

    Update header

--- src/components/common/layouts/Header.js
+++ src/components/common/layouts/Header.js
@@ -11,7 +11,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 
 const pages = ['Events', 'Awards', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
@@ -94,7 +93,6 @@ function Header() {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -111,7 +109,7 @@ function Header() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            LEO CLUB OF BHAKTAPUR NAMASTE
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
