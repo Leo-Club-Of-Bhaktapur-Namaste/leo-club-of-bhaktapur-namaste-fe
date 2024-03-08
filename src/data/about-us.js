662c74ba896106649aa0632b0968bf64b8827925
susankoju <askusankoju@gmail.com>
2024-03-08 17:22:07 +0545

    update about description

--- src/data/about-us.js
+++ src/data/about-us.js
@@ -5,8 +5,18 @@ import youth from '../assets/images/AMN04602.jpg'
 export const aboutTimeline = [
     {
         title: "Leo Club Of Bhaktapur Namaste",
-        description: "Leos who are ready to continue their service journey as Lions have a unique opportunity to enjoy many benefits and opportunities serving as Leo-Lion members. \n\nCurrent and former Leos between the age of majority through age 35 can access discounts and exciting programs through. \nCurrent and former Leos between the age of majority through age 35 can access discounts and exciting programs through the Leo-Lion membership program. the Leo-Lion membership program.",
+        description: "Bhaktapur Namaste Leo Club was established on November 25th, 2021, sponsored by the Lions Club of Bhaktapur Namaste. \nWith a commitment to youth engagement, leadership cultivation, social and community service, fellowship, youth advocacy for change, and self-development, the club endeavors to foster connections worldwide as social volunteers. \n\nOperating under the motto of Lions Club International, \"We Serve,\" the club is dedicated to making a positive impact both locally and globally.",
         image: leo_club_of_bhaktapur_namaste
+    },
+    {
+        title: "Lions Club of Bhaktapur Namaste",
+        description: "Lions Club of Bhaktapur Namaste was established in 2020.\nBhaktapur Namaste Leo Club was sponsored by the club in 2021.\n\nA social organization, collaborated to serve community thru liins norms \"we serve\".",
+        image: youth
+    },
+    {
+        title: "Leo clubs",
+        description: 'Leo clubs are a youth organization of Lions Clubs International.\n\nLeo clubs encourage youths to develop leadership qualities by participating in social service activities. They are dependent on a Lions club to sponsor and initiate a Leo club. Leo club members are addressed as "Leos."\nThey conduct various projects in the fields of health care, elders, children, disabled people, literacy and education, and self-development. Leos can raise funds by conducting fund-raising projects. They can conduct projects with another Leo club, sponsoring Lions club, or with an outside organization.\n\nLeo clubs are sponsored by Lions clubs and comprise an official program of Lions Clubs International.',
+        image: youth
     },
     {
         title: "2021-01-01",
@@ -22,10 +32,5 @@ export const aboutTimeline = [
         title: "2024-01-01",
         description: "2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 \n\n text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here",
         image: youth
-    },
-    {
-        title: "2024-02-01",
-        description: "2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 \n\n text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here 2024 text here",
-        image: youth
-    },
+    }
 ]
