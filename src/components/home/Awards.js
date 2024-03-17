fcbc7e27bc065d8b5f249eb19d9f3cbbf7834542
susankoju <askusankoju@gmail.com>
2024-03-17 07:26:13 +0545

    Update button

--- src/components/home/Awards.js
+++ src/components/home/Awards.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Container, Typography } from "@mui/material";
 import { awards } from 'data/awards';
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/swiper.min.css'
+import 'swiper/swiper-element-bundle.css'
 import 'swiper/modules/pagination/pagination.min.css'
+import './swiper.css'
 
 // import required modules
-import { Autoplay, FreeMode, Navigation } from 'swiper';
+import { Autoplay, Navigation } from 'swiper';
 
 const Awards = () => {
     return (
@@ -18,63 +20,53 @@ const Awards = () => {
 
                 <Swiper
                     slidesPerView={1}
+                    spaceBetween={10}
+                    autoplay={true}
+                    loop={true}
                     breakpoints={{
                         480: {
                             slidesPerView: 2,
                             spaceBetween: 20
                         },
-                        640: {
+                        820: {
                             slidesPerView: 3,
                             spaceBetween: 30
                         },
-                        820: {
+                        1100: {
                             slidesPerView: 4,
                             spaceBetween: 40
                         }
                     }}
-                    spaceBetween={30}
-                    autoplay={true}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    loop={true}
-                    navigation={{
-                        nextEl: ".swiper-button-next",
-                        prevEl: ".swiper-button-prev",
-                    }}
-                    modules={[FreeMode, Navigation, Autoplay]}
+                    navigation={true}
+                    modules={[Navigation, Autoplay]}
                     className="mySwiper"
                 >
-                    <Grid container justifyContent="center" direction="row" spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 16 }}>
-                        {awards.slice(0, 6).map((award, key) => (
-                            <SwiperSlide>
-                                <Grid item xs={4} sm={4} md={4} key={key}>
-                                    <Card style={{ borderRadius: "16px" }}>
-                                        <CardActionArea>
-                                            <CardMedia
-                                                component="img"
-                                                height="180"
-                                                image={award.image}
-                                                alt={award.title}
-                                            />
-                                            <CardContent>
-                                                <Typography gutterBottom variant="h5" component="div">
-                                                    {award.title}
-                                                </Typography>
-                                                <Typography height={60} variant="body2" color="text.secondary" align='left'>
-                                                    {award.description}
-                                                </Typography>
-                                                <Typography color="text.secondary" align='right'>
-                                                    {award.date}
-                                                </Typography>
-                                            </CardContent>
-                                        </CardActionArea>
-                                    </Card>
-                                </Grid>
-                            </SwiperSlide>
-                        ))}
+                    {awards.map((award, key) => (
+                        <SwiperSlide key={key}>
+                            <Card style={{ borderRadius: "16px" }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        height="180"
+                                        image={award.image}
+                                        alt={award.title}
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {award.title}
+                                        </Typography>
+                                        <Typography height={60} variant="body2" color="text.secondary" align='left'>
+                                            {award.description}
+                                        </Typography>
+                                        <Typography color="text.secondary" align='right'>
+                                            {award.date}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </SwiperSlide>
+                    ))}
 
-                    </Grid>
                 </Swiper>
             </div>
         </Container >
